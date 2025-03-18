fcd92ecb9b3cc008339299ab75ecd31fca19c485
Frank-xD <sandy.demoldo@gmail.com>
2025-03-18 01:31:48 -0400

    Funcion factorial

--- factorial.py
+++ factorial.py
@@ -1,1 +1,13 @@
-##estuvo aqui
+def factorial(a): 
+    b = a
+    res = 1
+    if a==0:
+        return 1
+    else: 
+        for i in range(b):
+            res = res*a
+            a = a-1  
+    return res
+
+#num1 = int(input("Ingrese el un número: "))
+#print(factorial(num1))
